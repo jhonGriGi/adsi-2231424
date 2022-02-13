44bf05a0f32416e532eb96de07cbc1d0235c5554
jhonGriGi <darwingrisalesg@gmail.com>
2022-02-13 14:21:37 -0500

    Update all files and add bootstrap exercises

--- Yaneth/ADSI/Clase 1/IDECS/form4.js
+++
@@ -1,7 +0,0 @@
-function capturaValoresInput() {
-    let capturaNombre = document.getElementById('user_name').value;
-    document.getElementById('verNombre').innerHTML = capturaNombre;
-
-    let capturaEmail = document.getElementById('user_email').value;
-    document.getElementById('verEmail').innerHTML = capturaEmail;
-}
